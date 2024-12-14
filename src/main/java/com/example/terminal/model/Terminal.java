aae59b2fd225c5de24f647f822f437470e0aaea6
Pagend <pariy.illya@gmail.com>
2024-12-14 11:19:23 +0200

    add lab3

---
+++ src/main/java/com/example/terminal/model/Terminal.java
@@ -0,0 +1,21 @@
+package com.example.terminal.model;
+
+import com.example.terminal.pattern.TerminalStrategy;
+
+public class Terminal {
+    private TerminalStrategy terminalStrategy;
+    public Terminal() {
+
+    }
+    public Terminal(TerminalStrategy terminalStrategy) {
+        this.terminalStrategy = terminalStrategy;
+    }
+
+    public void executeCommand(String command) {
+        terminalStrategy.executeCommand(command);
+    }
+
+    public void setTerminalStrategy(TerminalStrategy terminalStrategy) {
+        this.terminalStrategy = terminalStrategy;
+    }
+}
---
+++ src/main/java/com/example/terminal/pattern/TerminalStrategy.java
@@ -0,0 +1,5 @@
+package com.example.terminal.pattern;
+
+public interface TerminalStrategy {
+    void executeCommand(String command);
+}
